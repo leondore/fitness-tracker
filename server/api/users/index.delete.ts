34b2140d44521a512f43b1a159a8cb99a4f45acc
Leon Dore <leon.dore@gmail.com>
2023-10-31 09:36:10 -0400

    Continue building server admin route guard: grab token out of headers and throw error if token is missing

--- server/api/users/index.delete.ts
+++ server/api/users/index.delete.ts
@@ -1,23 +1,34 @@
 import { eq } from 'drizzle-orm';
 import { db } from '../../utils/db';
 import { handleError } from '../../utils/helpers';
-import authRoutes from '~/server/authRoutes';
 import { users, type User } from '~/db/schema';
 
 export default defineEventHandler(async (event) => {
-  const token = getHeader(event, 'Cookie');
-  await authRoutes(event);
-
   const { id } = await readBody<User>(event);
 
   try {
-    const deleted: User[] = await db
-      .delete(users)
-      .where(eq(users.id, id))
-      .returning();
+    let token: string | undefined;
+    const TOKEN_KEY = 'sb-access-token=';
+    const cookies = getHeader(event, 'Cookie');
+    const tokenKeyIdx = cookies?.indexOf(TOKEN_KEY);
+    if (typeof tokenKeyIdx === 'number' && tokenKeyIdx > -1) {
+      const tokenKey = cookies?.substring(tokenKeyIdx + TOKEN_KEY.length);
+      token = tokenKey?.substring(0, tokenKey.indexOf(';'));
+    }
+
+    if (!token) {
+      throw createError({
+        statusCode: 401,
+        message: 'Unauthorized',
+      });
+    }
+    // const deleted: User[] = await db
+    //   .delete(users)
+    //   .where(eq(users.id, id))
+    //   .returning();
 
     setResponseStatus(event, 200, 'OK');
-    return deleted;
+    return { token };
   } catch (err) {
     handleError(err);
   }
