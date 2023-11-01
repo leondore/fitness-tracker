eeaef25ce086ce5aeb08608b8939be5025dcaef7
Leon Dore <leon.dore@gmail.com>
2023-11-01 06:45:09 -0400

    Edit handleError server helper to get status code from error if error is H3Error

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -1,13 +1,22 @@
+import { H3Error } from 'h3';
+
 export function handleError(
   err: unknown,
   defaultMsg: string = 'Internal Server Error'
 ) {
   let message = defaultMsg;
+  let code = 500;
+
   if (err instanceof Error) {
     message = err.message;
   }
+
+  if (err instanceof H3Error) {
+    code = err.statusCode;
+  }
+
   return createError({
-    status: 500,
+    status: code,
     statusMessage: message,
   });
 }
