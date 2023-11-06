12efecda96b77e907aa5590100e8c48fa3b00a43
Leon Dore <leon.dore@gmail.com>
2023-11-06 16:58:54 -0400

    - Created admin user server route guard - Created delete user server route

--- server/adminRoutes.ts
+++ server/adminRoutes.ts
@@ -1,6 +1,11 @@
 import type { H3Event, EventHandlerRequest } from 'h3';
+import { eq } from 'drizzle-orm';
+import { jwtVerify } from 'jose';
 import type { User } from '@supabase/supabase-js';
 import { serverSupabaseUser } from '#supabase/server';
+import { db } from '~/server/utils/db';
+import { users } from '~/db/schema';
+import { Role } from '~/types/auth';
 
 declare module 'h3' {
   interface H3EventContext {
@@ -8,14 +13,40 @@ declare module 'h3' {
   }
 }
 
-export default async (event: H3Event<EventHandlerRequest>) => {
-  const user = await serverSupabaseUser(event);
+const unauthorizedError = createError({
+  statusCode: 401,
+  message: 'Unauthorized',
+});
 
-  if (!user) {
-    throw createError({
-      statusCode: 401,
-      message: 'Unauthorized',
-    });
+export default async (event: H3Event<EventHandlerRequest>) => {
+  let token: string | undefined;
+  const TOKEN_KEY = 'sb-access-token=';
+  const cookies = getHeader(event, 'Cookie');
+  const tokenKeyIdx = cookies?.indexOf(TOKEN_KEY);
+  if (typeof tokenKeyIdx === 'number' && tokenKeyIdx > -1) {
+    const tokenKey = cookies?.substring(tokenKeyIdx + TOKEN_KEY.length);
+    token = tokenKey?.substring(0, tokenKey.indexOf(';'));
+  }
+
+  if (!token) {
+    throw unauthorizedError;
+  }
+
+  const { jwtSecret } = useRuntimeConfig();
+  const secret = new TextEncoder().encode(jwtSecret);
+  const { payload } = await jwtVerify(token, secret);
+
+  if (!payload.aud || !payload.sub || payload.aud !== 'authenticated') {
+    throw unauthorizedError;
+  }
+
+  const user = await serverSupabaseUser(event);
+  const dbUser = await db.query.users.findFirst({
+    where: eq(users.id, payload.sub),
+  });
+
+  if (!user || !dbUser || dbUser.roleId !== Role.Admin) {
+    throw unauthorizedError;
   }
 
   event.context.auth = user;
--- server/api/users/index.delete.ts
+++ server/api/users/index.delete.ts
@@ -1,39 +1,30 @@
 import { eq } from 'drizzle-orm';
-import { jwtVerify } from 'jose';
 import { db } from '../../utils/db';
 import { handleError } from '../../utils/helpers';
+import { serverSupabaseServiceRole } from '#supabase/server';
 import { users, type User } from '~/db/schema';
+import adminRoutes from '~/server/adminRoutes';
 
 export default defineEventHandler(async (event) => {
+  await adminRoutes(event);
+
   const { id } = await readBody<User>(event);
 
   try {
-    let token: string | undefined;
-    const TOKEN_KEY = 'sb-access-token=';
-    const cookies = getHeader(event, 'Cookie');
-    const tokenKeyIdx = cookies?.indexOf(TOKEN_KEY);
-    if (typeof tokenKeyIdx === 'number' && tokenKeyIdx > -1) {
-      const tokenKey = cookies?.substring(tokenKeyIdx + TOKEN_KEY.length);
-      token = tokenKey?.substring(0, tokenKey.indexOf(';'));
-    }
+    const deleted: User[] = await db
+      .delete(users)
+      .where(eq(users.id, id))
+      .returning();
 
-    if (!token) {
-      throw createError({
-        statusCode: 401,
-        message: 'Unauthorized',
-      });
-    }
+    const client = serverSupabaseServiceRole(event);
+    const { error } = await client.auth.admin.deleteUser(id);
 
-    const { jwtSecret } = useRuntimeConfig();
-    const secret = new TextEncoder().encode(jwtSecret);
-    const { payload } = await jwtVerify(token, secret);
-    // const deleted: User[] = await db
-    //   .delete(users)
-    //   .where(eq(users.id, id))
-    //   .returning();
+    if (error) {
+      throw createError(error);
+    }
 
     setResponseStatus(event, 200, 'OK');
-    return { payload };
+    return deleted;
   } catch (err) {
     return handleError(err);
   }
